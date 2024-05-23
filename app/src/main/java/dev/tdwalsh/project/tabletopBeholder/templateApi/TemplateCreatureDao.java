1275bef15d24334b1e8859bbf028e778b86565b9
Tom Walsh <walsth@gmail.com>
2024-05-23 11:41:45 -0500

    Finished TemplateCreatureDao

--- app/src/main/java/dev/tdwalsh/project/tabletopBeholder/templateApi/TemplateCreatureDao.java
+++ app/src/main/java/dev/tdwalsh/project/tabletopBeholder/templateApi/TemplateCreatureDao.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.tdwalsh.project.tabletopBeholder.exceptions.CurlException;
 import dev.tdwalsh.project.tabletopBeholder.exceptions.MissingResourceException;
 import dev.tdwalsh.project.tabletopBeholder.templateApi.model.TemplateCreature;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -13,6 +15,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class responsible for taking in query parameters and returning results as {@link TemplateCreature}.
@@ -55,7 +59,50 @@ public class TemplateCreatureDao {
                     throw new MissingResourceException("5E API could not return resource: " + creatureSlug);
                 case 200:
                     return objectMapper.readValue(response.body(), TemplateCreature.class);
-                    //return templateCreatureConverter.unconvert(response.body());
+                default:
+                    throw new CurlException("Error making call to 5E API: " + response.statusCode() + "  " + response.body());
+            }
+        } catch (IOException e) {
+            throw new CurlException("Error making call to 5E API: ", e);
+        } catch (InterruptedException e) {
+            throw new CurlException("Call to 5E API interrupted: ", e);
+        }
+    }
+
+    /**
+     * Retrieves multiple {@link TemplateCreature} from an api call.
+     *
+     * @param searchTerms A list of search modifiers to be appended to the uri
+     * @return A list of {@link TemplateCreature} if found, or an empty list if none found
+     */
+    public List<TemplateCreature> getMultiple(String searchTerms)  {
+        //First, uses the search term chain of the external object to build a uri
+        //Then, makes a GET call with that url
+        //Then, converts the result to a list of model objects and returns it
+        HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URI_PATH + "?" + searchTerms))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+        try{
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            switch (response.statusCode()) {
+                case 404:
+                    throw new MissingResourceException("5E API could not return resource with parameters: " + searchTerms);
+                case 200:
+                    JSONObject creatureJson = new JSONObject(response.body());
+                    JSONArray creatureArray =  creatureJson.getJSONArray("results");
+                    List<TemplateCreature> templateCreatureList = new ArrayList<>();
+                    for(int i = 0; i < creatureArray.length(); i++) {
+                        templateCreatureList.add(objectMapper.readValue(creatureArray.get(i).toString(), TemplateCreature.class));
+                    }
+                    return templateCreatureList;
                 default:
                     throw new CurlException("Error making call to 5E API: " + response.statusCode() + "  " + response.body());
             }
